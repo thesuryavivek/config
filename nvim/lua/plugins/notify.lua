a492f0dae400e171c53349801a89ff9454272871
Surya Vivek <suryavivekofficial@gmail.com>
2023-11-26 16:44:47 +0530

    monaspace nerd font

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -2,6 +2,8 @@ return {
   "rcarriga/nvim-notify",
 
   config = function()
-    vim.notify = require("notify")
+    vim.notify = require("notify").setup({
+      background_colour = "#000000",
+    })
   end,
 }
